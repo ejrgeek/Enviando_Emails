3cb498212dc6531742788a6211bdd91c3c459f74
ejr_geek <ejrflaut@hotmail.com>
2016-12-15 22:42:08 -0300

    Envio de Emails(SMTP e SSL)
    Yahoo Gmail Outlook

---
+++ Envio_Email_Outlook.py
@@ -0,0 +1,23 @@
+#coding: UTF-8
+import smtplib
+server = smtplib.SMTP('smtp.live.com', 587)
+server.ehlo()
+server.starttls()
+
+user = str(input("\033[1;34mSeu Email > "))
+senha = str(input("Sua Senha > "))
+para = str(input("Remetente > "))
+numero = int(input("\033[33mTotal de E-mails > "))
+msg = str(input("\033[36mInsira sua Mensagem > "))
+
+try:
+	server.login(user, senha)
+	print("\033[32mLogando")
+except:
+	print("\033[31mEmail ou Senha errados || Permissao Negada")
+
+x = 0
+while(x<numero):
+	server.sendmail(user, para, msg)
+	x=x+1
+	print("\033[36mEnviado", x, "Email")
---
+++ Envio_Email_Yahoo.py
@@ -0,0 +1,22 @@
+#coding: UTF-8
+import smtplib
+server = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465)
+server.ehlo()
+
+user = str(input("\033[1;34mSeu Email > "))
+senha = str(input("Sua Senha > "))
+para = str(input("Remetente > "))
+numero = int(input("\033[33mTotal de E-mails > "))
+msg = str(input("\033[36mInsira sua Mensagem > "))
+
+try:
+	server.login(user, senha)
+	print("\033[32mLogando")
+except:
+	print("\033[31mEmail ou Senha errados || Permissao Negada")
+
+x = 0
+while(x<numero):
+	server.sendmail(user, para, msg)
+	x=x+1
+	print("\033[36mEnviado", x, "Email")
